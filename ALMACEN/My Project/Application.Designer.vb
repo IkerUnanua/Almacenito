a372ae8d83d92647a142c099000e2ce1cb096273
Arkyy <97agalisteo@sanferminikastola.org>
2017-05-15 13:50:33 +0200

    15/05/2017

--- ALMACEN/My Project/Application.Designer.vb
+++ ALMACEN/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ALMACEN.Juego
+            Me.MainForm = Global.ALMACEN.index
         End Sub
     End Class
 End Namespace
